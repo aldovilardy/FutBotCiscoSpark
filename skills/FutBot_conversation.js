e70cd7b05253e691baf255ff73a7b48f87001528
Aldo Fernando Vilardy Roa <aldovilardy@gmail.com>
2018-04-25 07:52:12 -0500

    panini sticker generation

--- skills/FutBot_conversation.js
+++ skills/FutBot_conversation.js
@@ -1,3 +1,7 @@
+var validator = require("email-validator");
+var date_format = require('dateformat');
+fs = require('fs');
+const exec = require('child_process').exec;
 module.exports = function (controller) {
 
     controller.hears(['jugar'], 'direct_message,direct_mention', function (bot, message) {
@@ -31,7 +35,7 @@ module.exports = function (controller) {
 
             convo.beforeThread('say-eMail', function (convo, next) {
                 var eMail = convo.extractResponse('eMail');
-                var validator = require("email-validator");
+
                 if (validator.validate(eMail)) {
                     convo.setVar('eMail', eMail);
                     next();
@@ -57,30 +61,30 @@ module.exports = function (controller) {
             }, function (res, convo) {
                 // photo has been collected...
                 if (res.data.files) {
-                    bot.retrieveFileInfo(res.data.files[0], function (err, file_info) {
-                        if (file_info['content-type'].includes('image/')) {
 
-                            convo.setVar('photoInfo', {
-                                'filename': file_info.filename,
-                                'content-type': file_info['content-type'],
-                                'content-length': file_info['content-length'],
-                                'cache-control': file_info['cache-control'],
-                                'connection': file_info['connection'],
-                                'content-disposition': file_info['content-disposition'],
-                                'date': file_info['date'],
-                                'l5d-success-class': file_info['l5d-success-class'],
-                                'server': file_info['server'],
-                                'strict-transport-security': file_info['strict-transport-security'],
-                                'trackingid': file_info['trackingid'],
-                                'via': file_info['via'],
-                                'ciscoSparkUrl': res.data.files[0]
+                    bot.retrieveFileInfo(res.data.files[0], function (err, file_info) {
+                        var validFile = (file_info['content-type'].includes('image/'));
+                        if (file_info['content-type'].includes('image/')) {
+                            console.log('Antes de retrieve!');
+                            bot.retrieveFile(res.data.files[0], function (err, file) {
+                                //
+                                // Write file to filesystem
+                                //
+                                console.log('Entro a Retieve!');
+                                var response = writePhoto('public/AttachedFiles/', file_info['filename'], file);
+                                convo.setVar('internalUrlPhoto', `${process.env.PUBLIC_URL}/${file_info.filename}`);
+                                convo.setVar('internalPhotoFileName', file_info.filename);
+                                console.log('Va para confirm-photo!');
+                                convo.gotoThread('confirm-photo');
                             });
-                            convo.gotoThread('confirm-photo');
+
+                            // convo.gotoThread('confirm-photo');
                         }
                         else {
                             convo.gotoThread('error-photo');
                         }
                     });
+
                 }
                 else {
                     convo.say(`No se puede capturar adjunto.`);
@@ -93,8 +97,74 @@ module.exports = function (controller) {
                 action: 'ask-photo'
             }, 'error-photo');
 
+            convo.beforeThread('confirm-photo', function (convo, next) {
+
+                //
+                // Create the sticker
+                // Windows style
+                //
+                var sticker_photo_input = 'public/AttachedFiles/' + convo.vars.internalPhotoFileName;
+                var sticker_photo_output = 'public/AttachedFiles/' + convo.vars.internalPhotoFileName.slice(0, -4) + "_sticker.png";
+                var sticker_command_first = 'convert public/AttachedFiles/mask_col.png \( "' + sticker_photo_input + '" -resize 1536x2048^ \) -compose overlay -composite public/AttachedFiles/mask_col.png -composite public/AttachedFiles/outtemp.png';
+
+                var sticker_command_last = 'convert public/AttachedFiles/outtemp.png -font Whitney-Semibold -weight 700  -pointsize 70 -draw "fill black text 300,1860 \'' + convo.vars.name.toUpperCase();
+                sticker_command_last += '\' " -pointsize 50 -draw "gravity northeast fill black text 100,1900 \'' + 'CALLTECH S.A.';
+                sticker_command_last += '\' " -pointsize 50 -draw "gravity northeast fill black text 800,1710 \'' + '20-04-2018';
+                sticker_command_last += '\' " -pointsize 50 -draw "gravity northeast fill black text 200,315 \'' + '2018';
+                sticker_command_last += '\' " ' + sticker_photo_output;
+
+                //
+                // Execute the first command
+                //
+                console.log(">>>Command1 Start: " + date_format(new Date(), "h:MM:ss"));
+                console.log("Command: " + sticker_command_first);
+                var imageMagick_first = exec(sticker_command_first,
+                    (error, stdout, stderr) => {
+                        console.log(`${stdout}`);
+                        console.log(`${stderr}`);
+                        console.log(">>>Command1 End: " + date_format(new Date(), "h:MM:ss"));
+
+                        //
+                        // Execute the second public/AttachedFiles/outtemp.png
+                        //
+                        console.log(">>>Command2 Start: " + date_format(new Date(), "h:MM:ss"));
+                        console.log("Command: " + sticker_command_last);
+                        var imageMagick_last = exec(sticker_command_last,
+                            (error, stdout, stderr) => {
+                                console.log(`${stdout}`);
+                                console.log(`${stderr}`);
+                                console.log(">>>Command End: " + date_format(new Date(), "h:MM:ss"));
+
+                                //
+                                // Save the Sticker Image
+                                //
+                                convo.setVar('internalUrlSticker', `${process.env.PUBLIC_URL}/${convo.vars.internalPhotoFileName.slice(0, -4)}_sticker.png`);  
+                                var dummy;                              
+
+                                if (error !== null) {
+                                    console.log(`exec error: ${error}`);
+                                    convo.gotoThread('error-photo');
+                                }
+                            });
+
+                        if (error !== null) {
+                            console.log(`exec error: ${error}`);
+                            convo.gotoThread('error-photo');
+                        }
+                        else{
+                            next();
+                        }
+                    });
+                
+
+            }).catch(function (err) {
+                convo.setVar('error', err);
+                convo.gotoThread('error-photo');
+                next(err); // pass an error because we changed threads again during this transition
+            });
+
             convo.addMessage({
-                text: '{{vars.name}}, el nombre de la imagen es: {{vars.photoInfo.filename}}. La URL cisco spark para trabajar es: {{vars.photoInfo.ciscoSparkUrl}}'
+                text: '{{vars.name}}, <BR/>La URL interna de la foto es: {{vars.internalUrlPhoto}}. <BR/>La URL del Sticker es: {{vars.internalUrlSticker}}'
             }, 'confirm-photo');
 
             // now, define a function that will be called AFTER the `default` thread ends and BEFORE the `completed` thread begins
@@ -104,3 +174,13 @@ module.exports = function (controller) {
     });
 }
 
+
+function writePhoto(path, filename, buffer) {
+    fs.writeFile(path + filename, buffer, 'binary', function (err) {
+        if (err) throw err;
+        console.log('It\'s saved!');
+
+    });
+
+    return true;
+}
